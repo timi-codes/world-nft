1806d9b2e47ff1f360f65670727147ca005859db
--replace <tejumoladavid@gmail.com>
2024-05-13 23:58:50 -0400

    feat(backend-api): implment buy citizenship endpoint

--- backend/src/controller/citizenship.controller.ts
+++ backend/src/controller/citizenship.controller.ts
@@ -1,21 +1,36 @@
 import { RequestHandler } from "express";
+import Web3 from 'web3'
 
 interface CitizenPayload {
-    wallet: string;
-    token_id: number;
+    signed_trx_hash: string;
 }
 
 interface ResponseData {
     success: boolean;
     message: string;
     data?: any;
+
 }
+
+const web3 = new Web3(process.env.WEB3_PROVIDER as string)
 
 export const joinCitizenship: RequestHandler<{}, ResponseData, CitizenPayload, {}> = async (req, res) => {
     try {
-        const data = req.body
-        //TODO: call smart contract to add a citizen to a continent
-        return res.send({ success: true, message: "Citizen added successfully", data: {} })
+        const { signed_trx_hash } = req.body
+        
+        const trx = await web3.eth.sendSignedTransaction(signed_trx_hash)
+
+        return res.send({
+            success: true,
+            message: "Citizen added successfully",
+            data: {
+                trx_hash: trx.transactionHash,
+                block_number: trx.blockNumber,
+                from: trx.from,
+                to: trx.to,
+                status: trx.status
+            }
+        });
     } catch (err: any) {
         return res.status(400).send({ success: false, message: err.message })
     }
