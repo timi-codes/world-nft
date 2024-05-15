2c1ec582e8d3b6b22444b71ed5dc0d0b63808b19
--replace <tejumoladavid@gmail.com>
2024-05-15 16:50:36 -0400

    hotfix(dapp): fix ssr issue with wallet connect kit

--- client/src/providers/Web3Provider.tsx
+++ client/src/providers/Web3Provider.tsx
@@ -1,14 +1,15 @@
 "use client"
 
 import { WagmiProvider, createConfig, http } from "wagmi";
-import { mainnet } from "wagmi/chains";
+import { mainnet, base } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConnectKitProvider, getDefaultConfig } from "connectkit";
 import { ReactNode } from "react";
 
 const config = createConfig(
     getDefaultConfig({
-        chains: [mainnet],
+        chains: [mainnet, base],
+        ssr: true,
         transports: {
             [mainnet.id]: http(
                 `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`,
