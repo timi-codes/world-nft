9743e6e59d32bf9f97557e739ea279280024f2ff
--replace <tejumoladavid@gmail.com>
2024-05-19 01:51:26 -0400

    feat(frontend): update supported chains

--- client/src/providers/Web3Provider.tsx
+++ client/src/providers/Web3Provider.tsx
@@ -1,18 +1,18 @@
 "use client"
 
 import { WagmiProvider, createConfig, http } from "wagmi";
-import { mainnet, base } from "wagmi/chains";
+import { mainnet, base, baseSepolia } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConnectKitProvider, getDefaultConfig } from "connectkit";
 import { ReactNode } from "react";
 
 const config = createConfig(
     getDefaultConfig({
-        chains: [mainnet, base],
+        chains: [baseSepolia],
         ssr: true,
         transports: {
-            [mainnet.id]: http(
-                `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`,
+            [baseSepolia.id]: http(
+                `https://base-sepolia.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`,
             ),
         },
         walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
