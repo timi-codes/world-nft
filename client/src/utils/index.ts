54cc021ea252001a109a50e1fe96b3c90bfab49c
--replace <tejumoladavid@gmail.com>
2024-05-22 19:06:15 -0400

    fix(dapp): implement horizaontal scroll for continents card

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,5 +1,6 @@
+import { toast } from "sonner";
+
 export function shortenAddress(address: string) {
-    console.log(address)
     let shortenedAddress = address ? address.slice(0, 5) + "..." + address.slice(-5) : "";
     return shortenedAddress;
 }
@@ -25,14 +26,12 @@ export const transformIPFSURL = (url: string) => {
 };
 
 
-
 type HttpMethod = "GET" | "POST" | "PUT" | "DELETE";
 export type FetchAPIResponse<T> = {
     success: boolean;
     data: T;
     message?: string;
 };
-
 
 export const fetchAPI = async <T>(
     path: string,
@@ -50,3 +49,41 @@ export const fetchAPI = async <T>(
     const data = await response.json() as T;
     return data as FetchAPIResponse<T>;;
 };
+
+export const showError = (message: string ) => {
+
+    if (message.includes("insufficient funds")) {
+        toast.error("Insufficient funds", {
+            description: "You do not have enough funds to place this bid. Please top up your wallet and try again. The cost of transaction is calculated as `gas * gas fee + value`",
+            position: "top-left",
+        })
+    }
+
+    if (message.includes("Ownable: caller is not the owner")) {
+        toast.error("Transaction failed", {
+            description: "You can start an auction only if you own the continent",
+            position: "top-left",
+        });
+    }
+
+    if (message.includes("Continent does not exist")) {
+        toast.error("Transaction failed", {
+            description: "Continent does not exist",
+            position: "top-left",
+        });
+    }
+
+    if (message.includes("You already own this continent")) {
+        toast.error("Transaction failed", {
+            description: "You already own this continent",
+            position: "top-left",
+        });
+    }
+
+    if (message.includes("Not enough Ether sent to cover citizenship tax")) {
+        toast.error("Transaction failed", {
+            description: "Not enough Ether sent to cover citizenship tax",
+            position: "top-left",
+        });
+    }
+}
